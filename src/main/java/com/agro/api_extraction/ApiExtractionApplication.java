8924dc0a3acb5e2a47739dce7a6da6ef5cc7c7c2
Marcos Morais <marcosmorais.v10@gmail.com>
2025-06-08 23:14:29 -0300

    Initial Project

---
+++ src/main/java/com/agro/api_extraction/ApiExtractionApplication.java
@@ -0,0 +1,11 @@
+package com.agro.api_extraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExtractionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(com.agro.api_extraction.ApiExtractionApplication.class, args);
+    }
+}
---
+++ src/test/java/com/agro/api_extraction/ApiExtractionApplicationTests.java
@@ -0,0 +1,12 @@
+package com.agro.api_extraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApiExtractionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
